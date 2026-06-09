e5a90fd26f92f70f953cc922183e0717d28a80fb
Vincius Lessa Magagnin <viniciuslessamagagnin123@gmail.com>
2026-06-09 16:04:24 -0300

    Versão com views

--- models/genre.js
+++ models/genre.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const GenreSchema = new Schema ({
-    name: { type: String, minLenght: 3, maxLenght: 100}
-})
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
 
+// Virtual for this genre instance URL.
 GenreSchema.virtual("url").get(function () {
-    return `catalog/genre/${this._id}`
-})
+  return "/catalog/genre/" + this._id;
+});
 
-module.exports = mongoose.model("Genre", GenreSchema)
+// Export model.
+module.exports = mongoose.model("Genre", GenreSchema);
